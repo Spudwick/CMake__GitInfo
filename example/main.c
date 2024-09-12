bb89fdc38dfbb87cc63da2cc895ccf26142807b9
Spudwick <olivert321@gmail.com>
2024-09-12 22:59:27 +0100

    Updated example

--- example/main.c
+++ example/main.c
@@ -5,12 +5,26 @@
 
 int main( int argc, char *argv[] )
 {
-    time_t commit_tmstmp = GIT_COMMIT_TIMESTAMP;
+    const time_t commit_tmstmp = GIT_COMMIT_TIMESTAMP;
+    const char   commit_hash[] = GIT_COMMIT_HASH;
+    const char   branch[]      = GIT_BRANCH;
+    const char   dirty         = GIT_DIRTY;
+
+    printf("Branch : %s\n", branch);
+    printf("Commit : %s\n", commit_hash);
 
     char buff[20];
-    strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&commit_tmstmp));
+    strftime(buff, 20, "%d-%m-%Y %H:%M:%S", localtime(&commit_tmstmp));
+    printf("Commit Time : %s\n", buff);
 
-    printf("%s\n", buff);
+    if( GIT_DIRTY )
+    {
+        printf("Workspace DIRTY!\n");
+    }
+    else
+    {
+        printf("Workspace clean.\n");
+    }
 
     return 0;
 }
