0862b9e75f2b14b29c9e999f4fcc6166cfe9bfd6
Thomas Oliver <thomas.oliver@ignys.co.uk>
2025-10-15 10:45:20 +0100

    Modified dirty processing

--- example/main.c
+++ example/main.c
@@ -16,7 +16,7 @@ int main( int argc, char *argv[] )
     strftime(buff, 20, "%d-%m-%Y %H:%M:%S", localtime(&commit_tmstmp));
     printf("Commit Time : %s\n", buff);
 
-#ifdef GIT_DIRTY
+#if GIT_DIRTY
         printf("Workspace DIRTY!\n");
 #else
         printf("Workspace clean.\n");
